16fca50b753bcb37189817651fc2583d69805336
Carolinaanogues <88649795+Carolinaanogues@users.noreply.github.com>
2021-10-19 14:14:20 +0200

    corrigé le systeme UC, et corrigé des details sur la page login

--- manypages/manypages/Login.xaml.cs
+++ manypages/manypages/Login.xaml.cs
@@ -15,7 +15,7 @@ namespace manypages
     /// <summary>
     /// Logique d'interaction pour Login.xaml
     /// </summary>
-    public partial class Login : Window
+    public partial class Login : Page
     {
         public Login()
         {
--- manypages/manypages/Profile.xaml.cs
+++ manypages/manypages/Profile.xaml.cs
@@ -15,11 +15,16 @@ namespace manypages
     /// <summary>
     /// Logique d'interaction pour Profile.xaml
     /// </summary>
-    public partial class Profile : Window
+    public partial class Profile : Page
     {
         public Profile()
         {
             InitializeComponent();
+        }
+
+        private void btn_Profile_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new Profile());
         }
     }
 }
