1b429a48512917e77a6a6050269e7b8aaa7a0792
Carolinaanogues <88649795+Carolinaanogues@users.noreply.github.com>
2021-10-19 10:37:13 +0200

    lll

---
+++ manypages/manypages/Historique.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace manypages
+{
+    /// <summary>
+    /// Logique d'interaction pour Historique.xaml
+    /// </summary>
+    public partial class Historique : Window
+    {
+        public Historique()
+        {
+            InitializeComponent();
+        }
+    }
+}
