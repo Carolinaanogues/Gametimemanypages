bf7f509152743413548d93a1f22da60bf563d1d4
Julien Gunther <jgunther021@gmail.com>
2021-10-20 09:10:07 +0200

    Project restructuration

---
+++ Manypages/manypages/Views/Createaccount.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace manypages
+{
+    /// <summary>
+    /// Logique d'interaction pour Createaccount.xaml
+    /// </summary>
+    public partial class Createaccount : Page
+    {
+        public Createaccount()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Manypages/manypages/Views/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace manypages
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += MyWindow_Loaded;
+        }
+
+        private void MyWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            frame.NavigationService.Navigate(new Home());
+        }
+    }
+}
