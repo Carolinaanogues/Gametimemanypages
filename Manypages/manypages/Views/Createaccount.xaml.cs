40fc1ae8e81acf5d96f27b073ebefe8ccc6a747e
Chenxi <chenxi.lin@hotmail.ch>
2021-11-03 15:49:57 +0100

    new createaccount

--- Manypages/manypages/Views/Createaccount.xaml.cs
+++ Manypages/manypages/Views/Createaccount.xaml.cs
@@ -48,7 +48,7 @@ namespace manypages
                 return;
             }
 
-            lblErr2.Content = "Le pseudo existe déjà.";
+            lblErr1.Content = "Le pseudo existe déjà.";
 
 
         }
