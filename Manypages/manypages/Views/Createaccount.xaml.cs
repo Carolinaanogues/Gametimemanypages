90f81db2453ef213a5d445fbb31a9d29b353165b
Chenxi <chenxi.lin@hotmail.ch>
2021-11-04 11:55:26 +0100

    commentaire createaccount

--- Manypages/manypages/Views/Createaccount.xaml.cs
+++ Manypages/manypages/Views/Createaccount.xaml.cs
@@ -12,6 +12,7 @@ namespace manypages
         public ModelJeux Jeux { get; set; }
         public ModelHistorique Historique { get; set; }
 
+        // Constructor
         public Createaccount(ModelProfiles mp, ModelJeux mj, ModelHistorique mh)
         {
             Profiles = mp;
@@ -20,6 +21,11 @@ namespace manypages
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Bouton retour sur la page Login
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btn_Home_Click(object sender, System.Windows.RoutedEventArgs e)
         {
             NavigationService?.Navigate(new Login(Profiles, Jeux, Historique));
@@ -27,8 +33,14 @@ namespace manypages
 
         #region CreateProfile
 
+        /// <summary>
+        /// Création d'un neauvou profil
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void CreateAccount(object sender, System.Windows.RoutedEventArgs e)
         {
+            // Vérification des champs s'ils sont bien remplis
             if (CheckEmpty(txtNickname.Text) || CheckEmpty(pwdPassword.Password) || CheckEmpty(txtName.Text) ||
                 CheckEmpty(txtFirstName.Text) || CheckEmpty(dpBirthDate.Text) || CheckEmpty(txtEmail.Text))
             {
@@ -36,15 +48,17 @@ namespace manypages
                 return;
             }
 
+            // Vérifier selon le pseudo si l'utilisateur existe déjà, sinon insersion d'un nouveau compte dans ModelProfiles
             try
             {
                 Profiles.GetByUsername(txtNickname.Text);
             }
             catch (InvalidOperationException)
             {
-                //Profil NewProfile = new Profil(txtNickname.Text, txtName.Text, txtFirstName.Text, DateTime.Parse(dpBirthDate.Text), txtEmail.Text, pwdPassword.Password);
+
                 Profiles.Add(txtNickname.Text, txtName.Text, txtFirstName.Text, DateTime.Parse(dpBirthDate.Text),
                     txtEmail.Text, pwdPassword.Password);
+                // Redirection vers la page login après la création du compte
                 NavigationService?.Navigate(new Login(Profiles, Jeux, Historique));
                 return;
             }
@@ -52,6 +66,11 @@ namespace manypages
             lblErr1.Content = "Le pseudo existe déjà.";
         }
 
+        /// <summary>
+        /// Méthode pour vérifier le remplissage du champs
+        /// </summary>
+        /// <param name="txt"></param>
+        /// <returns></returns>
         private bool CheckEmpty(string txt)
         {
             return string.IsNullOrEmpty(txt);
